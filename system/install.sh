935da9fe20a8d5d916ed745b205ea8ad2e1652d5
dheera <dheera@dheera.net>
2019-06-07 00:37:41 -0700

    updat

--- system/install.sh
+++ system/install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+sudo apt-get update
+
 sudo apt-get install python-pip python3-pip
-sudo pip3 install --upgrade tornado socketio_client_nexus numpy pillow psutil
+cat pip-installs.txt | xargs sudo pip install --upgrade
+cat pip-installs.txt | xargs sudo pip3 install --upgrade
+
+curl https://raw.githubusercontent.com/dheera/jetson-install-this/master/ros.shit | bash
+curl https://raw.githubusercontent.com/dheera/jetson-install-this/master/pytorch.shit | bash
+curl https://raw.githubusercontent.com/dheera/jetson-install-this/master/tensorflow.shit | bash
+
